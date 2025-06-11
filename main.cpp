582d8973d4d3c5bc31ba08da1de26c8d9ffc305b
Lautaro De Lucia <ldelucia@fi.uba.ar>
2025-06-11 23:52:35 +0000

    update

--- main.cpp
+++ main.cpp
@@ -1,31 +1,51 @@
-#include "mbed.h"
-#include "arm_book_lib.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include "hal/gpio_api.h"
+#include "PinNames.h"
+#include "arm_book_lib.h"   
 
-int main()
+int main(void)
 {
-    DigitalIn gasDetector(D2);
-    DigitalIn overTempDetector(D3);
-    DigitalIn alarmOffButton(BUTTON1);
+    gpio_t gasDetector;
+    gpio_t overTempDetector;
+    gpio_t alarmOffButton;
+    gpio_t alarmLed;
 
-    DigitalOut alarmLed(LED1);
+    // Reemplaza: DigitalIn gasDetector(D2);
+    gpio_init_in(&gasDetector, D2);
+    gpio_mode(&gasDetector, PullDown);  // Reemplaza: gasDetector.mode(PullDown);
 
-    gasDetector.mode(PullDown);
-    overTempDetector.mode(PullDown);
+    // Reemplaza: DigitalIn overTempDetector(D3);
+    gpio_init_in(&overTempDetector, D3);
+    gpio_mode(&overTempDetector, PullDown);  // Reemplaza: overTempDetector.mode(PullDown);
 
-    alarmLed = OFF;
+    // Reemplaza: DigitalIn alarmOffButton(BUTTON1);
+    gpio_init_in(&alarmOffButton, BUTTON1);
 
+    // Reemplaza: DigitalOut alarmLed(LED1);
+    gpio_init_out_ex(&alarmLed, LED1, OFF);
+
+    // Reemplaza: bool alarmState = OFF;
     bool alarmState = OFF;
 
     while (true) {
 
-        if ( gasDetector || overTempDetector ) {
+        // Reemplaza: if ( gasDetector || overTempDetector )
+        if (gpio_read(&gasDetector) || gpio_read(&overTempDetector)) {
+            printf("Escape de gas o sobre-temperatura detectada.\n");
             alarmState = ON;
         }
+ 
+        // Reemplaza: alarmLed = alarmState;
+        gpio_write(&alarmLed, alarmState);
 
-        alarmLed = alarmState;
-
-        if ( alarmOffButton ) {
+        // Reemplaza: if ( alarmOffButton )
+        if (gpio_read(&alarmOffButton)) {
+            printf("Todo en orden.\n");
             alarmState = OFF;
         }
-    }
+
+        // Agregado
+        delay(100);
+    }
 }
